d12bbb645a4dbe8194903db384be2e15f6b524aa
Sid@30 <65735816+sidkjha@users.noreply.github.com>
2023-01-07 02:24:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+export {default as NavBar} from './Navbar';
+export {default as Feed} from './Feed';
+export {default as ChannelDetail} from './ChannelDetail';
+export {default as SearchFeed} from './SearchFeed';
+export {default as VideoDetail} from './VideoDetail';
+export {default as SerachBar} from './SerachBar';
+export {default as Sidebar} from './Sidebar';
+export {default as Videos} from './Videos';
